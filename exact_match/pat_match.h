fd2b705fc760cfe195cdb43176979a45b8b1f2b5
Lucas Peres <lucasperes95@gmail.com>
2023-08-20 20:37:21 -0300

    added preprocessing function headers to BM

--- exact_match/pat_match.h
+++ exact_match/pat_match.h
@@ -3,8 +3,14 @@
 #include <string.h>
 
 void z_array(const char *s, int n, int z[]);
-void z_sp_vals(const char *s, int n, int z[], int sp[]);
-void sp_vals(const char *s, int n, int sp[]);
+
+void z_sp_vals(const char *pat, int n, int z[], int sp[]);
+void sp_vals(const char *pat, int n, int sp[]);
 void fail_func(int sp[], int f[], int n);
 void kmp_pmatch(const char *pat, int n, const char *txt, int m, int f[]);
 void kmp_pmatch2(const char *pat, int n, const char *txt, int m, int sp[]);
+
+void N_vals(const char *pat, int n, int N[]);
+void L_vals(const char *pat, int n, int L[]);
+void l_vals(const char *pat, int n, int l[]);
+void bm_pmatch(const char *pat, int n, const char *txt, int m, int L[], int l[], int R[]);
